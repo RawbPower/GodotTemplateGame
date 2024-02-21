eeff7b51c18d0e3b6d7aa26500dfe51155d877c5
RawbPower <robjamespower@gmail.com>
2024-02-21 21:43:28 +0000

    Hitboxes and Hurtboxes

--- Base/Scripts/Breakable.gd
+++ Base/Scripts/Breakable.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 @export var break_effect : Resource
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var breakEffect = break_effect.instantiate()
-		var world = get_tree().current_scene
-		world.add_child(breakEffect)
-		breakEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var breakEffect = break_effect.instantiate()
+	var world = get_tree().current_scene
+	world.add_child(breakEffect)
+	breakEffect.global_position = global_position
+
+
+func _on_hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
